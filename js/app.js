998aeae014ea18045f017731fd4e6fb44834f44f
Irina Kramer <irakramer@gmail.com>
2020-04-27 20:01:41 -0700

    added choices for num of dice to roll, favicon

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
 /**
  * Pseudocode:
- * Generate random number 1-6 for variable random1
- * Generate random number 1-6 for variable random2
- * firstDie will be assigned a value 'dice-random1'
- * secondDie will be assigned a value 'dice-random2'
- * get the first die by ID and update its class to firstDie 
- * get the second die by ID and update its class to secondDie 
- * diceRoll function 
- * when user clicks the button the diceRoll funtion runs
- */
+ * Function to generate random numbers 1-6 - getRandomInt(min,max).
+ * Function to iterate over DOM dice elements and 
+ * set classes with the correct number dice,
+ * or to paint dark for non-playing dice - diceRoll(n).
+ * Set onclick event for all buttons and call diceRoll() function.
+ * */
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -16,46 +13,18 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-//   //console.log(getRandomInt(1, 7));
-
-//   let random1 = getRandomInt(1, 7);
-//   let random2 = getRandomInt(1, 7);
-//   const firstDie = 'dice-' + random1;
-//   const secondDie = 'dice-' + random2;
-
-//   // console.log(firstDie, secondDie);
-
-//   function diceRoll() {
-//     document.getElementById("first-die").className = "dice " + firstDie;
-//     document.getElementById("second-die").className = "dice " + secondDie;
-//   }
-//   document.getElementById("roll-dice").onclick = diceRoll;
-
-//   document.getElementById("roll-dice").addEventListener("click", function() {
-//     document.getElementById("first-die").className = "dice " + firstDie;
-//     document.getElementById("second-die").className = "dice " + secondDie;
-//   }); 
-
-/////////////
-
-function diceRoll() {
-    const random1 = getRandomInt(1, 7);
-    const random2 = getRandomInt(1, 7);
-    const random3 = getRandomInt(1, 7);
-    const random4 = getRandomInt(1, 7);
-    const random5 = getRandomInt(1, 7);
-    const firstDie = 'dice-' + random1;
-    const secondDie = 'dice-' + random2;
-    const thirdDie = 'dice-' + random3;
-    const fourthDie = 'dice-' + random4;
-    const fifthDie = 'dice-' + random5;
-
-    document.getElementById("first-die").className = "dice " + firstDie;
-    document.getElementById("second-die").className = "dice " + secondDie;
-    document.getElementById("third-die").className = "dice " + thirdDie;
-    document.getElementById("fourth-die").className = "dice " + fourthDie;
-    document.getElementById("fifth-die").className = "dice " + fifthDie;
+function diceRoll(n) {
+    for (let i = 1; i <= n; i++) {
+        const elemId = i.toString() + "-die";
+        document.getElementById(elemId).className = "dice " + "dice-" + getRandomInt(1, 7);
+    }
+    for (let j = (n + 1); j <= 5; j++) {
+        const elemIdDark = j.toString() + "-die";
+        document.getElementById(elemIdDark).className = "dice " + "dice-dark";
+    }
 }
-
-document.getElementById("roll-dice").onclick = diceRoll;
-
+document.getElementById("roll-five").onclick = () => diceRoll(5);
+document.getElementById("roll-four").onclick = () => diceRoll(4);
+document.getElementById("roll-three").onclick = () => diceRoll(3);
+document.getElementById("roll-two").onclick = () => diceRoll(2);
+document.getElementById("roll-one").onclick = () => diceRoll(1);
